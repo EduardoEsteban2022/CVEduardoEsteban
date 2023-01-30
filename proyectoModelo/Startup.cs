367628b8c77169d5ac4e39ad8eeffea39b94827a
EduardoEsteban <sharingan002019@gmail.com>
2023-01-30 11:59:34 -0600

    Uso de Versionador de git

---
+++ proyectoModelo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(proyectoModelo.Startup))]
+namespace proyectoModelo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
