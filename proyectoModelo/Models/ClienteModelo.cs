c2f75d563ad145065c4080ba0f72cf74cbebf7b8
EduardoEsteban <sharingan002019@gmail.com>
2023-02-27 10:09:34 -0600

    Se creo modelo de Cliente y la cadena de conexion en WebConfig

---
+++ proyectoModelo/Models/ClienteModelo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace proyectoModelo.Models
+{
+    public class ClienteModelo
+    {
+        [Display(Name = "Id")]
+        public int id { get; set; }
+
+        [Display(Name = "Nombres")]
+        [Required(ErrorMessage = "Los Nombres son requeridos.")]
+        public string nombres { get; set; }
+
+        [Display(Name = "Apellidos")]
+        [Required(ErrorMessage = "Los Apellidos son requeridos.")]
+        public string apellidos { get; set; }
+
+        [Display(Name = "Fecha Nacimiento")]
+        [Required(ErrorMessage = "La Fecha de nacimiento es requerido.")]
+        public DateTime fecha { get; set; }
+        
+        public string fechaString { get; set; }
+
+        [Display(Name = "Sueldo")]
+        [Required(ErrorMessage = "El Sueldo es requerido.")]
+        public decimal sueldo { get; set; }
+
+
+    }
+}
---
+++ proyectoModelo/Models/ClienteUtility.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace proyectoModelo.Models
+{
+    public class ClienteUtility
+    {
+        private SqlConnection con;
+
+        private void conexion()
+        {
+            string constr = ConfigurationManager.ConnectionStrings["Conectar"].ToString();
+            con = new SqlConnection(constr);
+        }
+
+
+
+    }
+}
